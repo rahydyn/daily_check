bba9009b48780c98fab7e699dc02e8a115308123
SatoruKanaoka <kanaokasatoru@gmail.com>
2019-02-21 07:56:29 +0000

    Add signup, login, logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,41 @@
 class UsersController < ApplicationController
     def new
+        @user=User.new
     end
+
     def create
+        @user=User.new(
+            name: params[:name],
+            email: params[:email],
+            password: params[:password]
+            )
+        if @user.save
+            session[:user_id]=@user.id
+            flash[:notice]="YOU HAVE SIGNED UP!"
+            redirect_to("/users/#{@user.id}")
+        else
+            render("users/new")
+        end
     end
+
     def show
+        @user=User.find_by(id: params[:id])
     end
     def login_form
     end
     def login
+        @user=User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id]=@user.id
+            flash[:notice]="YOU HAVE LOGGED IN!"
+            redirect_to("/users/#{@user.id}")
+        else
+            render("users/login_form")
+        end
     end
     def logout
+        session[:user_id]=nil
+        flash[:notic]="YOU HAVE LOGGED OUT!"
+        redirect_to("/login")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   
   get "signup", to:"users#new"
   post "users/create", to:"users#create"
-  get "users/show", to:"users#show"
   #get "users/:id/edit", to:"users#edit"
   #post "users/:id/update", to:"users#update"
   get "login", to:"users#login_form"
   post "login", to:"users#login"
   #get "users/index", to:"users#index"
   post "logout", to:"users#logout"
+  get "users/:id", to:"users#show"
   
   get "posts/new", to:"posts#new"
   post "posts/create", to:"posts#create"
